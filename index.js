79011d7534ae600d18fe0cda37a1ab4fd734cf88
NguyenHuy <nguyenquanghuy12a99@gmail.com>
2024-10-27 21:22:14 +0700

    image

--- index.js
+++ index.js
@@ -213,7 +213,7 @@ io.on("connection", (socket) => {
     console.log("Client disconnected");
   });
 });
-server.listen(3000, "0.0.0.0", () => {
+server.listen(5000, "0.0.0.0", () => {
   console.log("Server  is running on port 3000");
 });
 

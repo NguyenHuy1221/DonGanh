c8cfdd652fe36a9175ba3dc4b031fec5762bb9d6
NguyenHuy <nguyenquanghuy12a99@gmail.com>
2024-10-31 15:26:05 +0700

    image

--- index.js
+++ index.js
@@ -189,34 +189,6 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on('sendMessage', async ({ conversationId, text, imageUrl, videoUrl }) => {
-    try {
-      const message = new MessageModel({
-        text,
-        imageUrl,
-        videoUrl,
-        msgByUserId: userid,
-      });
-
-      // Gửi phản hồi nhanh chóng tới client
-      io.to(conversationId).emit('message', { conversationId, message });
-      // Lưu tin nhắn và cập nhật cuộc trò chuyện không đồng bộ
-      await message.save();
-
-      // Lưu tin nhắn vào cuộc hội thoại
-      const conversation = await ConversationModel.findById(conversationId);
-      conversation.messages.push(message._id);
-      await conversation.save();
-
-      // Gửi tin nhắn đến tất cả các client trong room
-      io.to(conversationId).emit('message', { conversationId, ...message.toObject() });
-    } catch (error) {
-      console.error('Error sending message:', error);
-      socket.emit('error', { message: 'An error occurred while sending the message' });
-    }
-  });
-
-
   // socket.on('sendMessage', async ({ conversationId, text, imageUrl, videoUrl }) => {
   //   try {
   //     const message = new MessageModel({
@@ -225,6 +197,10 @@ io.on("connection", (socket) => {
   //       videoUrl,
   //       msgByUserId: userid,
   //     });
+
+  //     // Gửi phản hồi nhanh chóng tới client
+  //     io.to(conversationId).emit('message', { conversationId, message });
+  //     // Lưu tin nhắn và cập nhật cuộc trò chuyện không đồng bộ
   //     await message.save();
 
   //     // Lưu tin nhắn vào cuộc hội thoại
@@ -239,6 +215,35 @@ io.on("connection", (socket) => {
   //     socket.emit('error', { message: 'An error occurred while sending the message' });
   //   }
   // });
+
+
+  socket.on('sendMessage', async ({ conversationId, text, imageUrl, videoUrl, IDSanPham }) => {
+    try {
+      //const now = Date.now();
+      const MIN_TIME_BETWEEN_MESSAGES = 1000;
+      const message = new MessageModel({
+        text,
+        imageUrl,
+        videoUrl,
+        IDSanPham,
+        msgByUserId: userid,
+      });
+
+      // Gửi phản hồi nhanh chóng tới client
+      io.to(conversationId).emit('message', { conversationId, message });
+      // Lưu tin nhắn và cập nhật cuộc trò chuyện không đồng bộ
+      await message.save();
+      const conversation = await ConversationModel.findById(conversationId);
+      //console.log(conversation)
+      conversation.messages.push(message._id);
+      await conversation.save();
+    } catch (error) {
+      console.error('Error sending message:', error);
+      socket.emit('error', { message: 'An error occurred while sending the message' });
+    }
+  });
+
+
   socket.on("disconnect", () => {
     console.log("Client disconnected");
   });
